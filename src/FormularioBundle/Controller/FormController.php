ed6509b9e78a7e1f085fb117130c7b91c14f958a
IhoFenixMFS <iho-ladamadeltiempo@live.com>
2018-03-08 13:29:52 +0100

    Buscar donde poner las vistas twig

--- src/FormularioBundle/Controller/FormController.php
+++ src/FormularioBundle/Controller/FormController.php
@@ -12,9 +12,9 @@ class FormController extends Controller
             $em = $this->getDoctrine()->getMAnager();
 
             $servicios = $em->getRepository('FormularioBundle:Servicios')->findAll();
-            $res = '<h1>hola</h1>';
-            return new Response ($res);
-           /* return $this->render('FormularioBundle:VistaTwig:index.html.twig', array('servicios'=> $servicios));*/
+           /* $res = '<h1>hola</h1>';
+            return new Response ($res);*/
+           return $this->render('FormularioBundle:index.html.twig', array('servicios'=> $servicios));
         }
 
     public function viewAction ()
